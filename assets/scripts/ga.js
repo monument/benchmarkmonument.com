f7958babf1fbfc50435142e3ce42bc27f36714de
Hawken Rives <hawkrives@gmail.com>
2012-02-04 16:27:52 -0600

    Added guag.es (to test)

--- assets/scripts/ga.js
+++ assets/scripts/ga.js
@@ -7,3 +7,15 @@ var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async
 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+
+var _gauges = _gauges || [];
+(function() {
+	var t   = document.createElement('script');
+	t.type  = 'text/javascript';
+	t.async = true;
+	t.id    = 'gauges-tracker';
+	t.setAttribute('data-site-id', '4f2db014844d52061000004e');
+	t.src = '//secure.gaug.es/track.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(t, s);
+})();
